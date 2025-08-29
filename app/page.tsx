4d3334c48217d6535d400695887e9ccf7cea3e55
samxzk70 <jiesern@raccoons.dev>
2025-08-29 09:41:03 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Hello is this saved?
+            No pre prod
           </li>
         </ol>
 
