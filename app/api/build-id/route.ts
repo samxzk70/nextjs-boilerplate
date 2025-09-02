57d5a9fedd0c1ef45e7d80320d65d9a147f743a4
samxzk70 <jiesern@raccoons.dev>
2025-09-02 18:36:31 +0800

    feat(app): test

--- app/api/build-id/route.ts
+++ app/api/build-id/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   return NextResponse.json({
-    buildId: process.env.BUILD_ID || "unknown",
+    buildId: process.env.NEXT_PUBLIC_VERCEL_DEPLOYMENT_ID || "unknown",
   })
 }
--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
-const nextBuildId = require("next-build-id");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  generateBuildId: async () => {
-    const id = await nextBuildId();
-    return id;
-  },
-  env: {
-    BUILD_ID: nextBuildId.sync(),
-  },
 };
 
 module.exports = nextConfig;
